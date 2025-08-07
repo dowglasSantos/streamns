8734294867e24a4317719064b45e0718be79ba0a
dowglasOficial <bittencourtdouglas41@gmail.com>
2025-08-07 20:00:52 -0300

    exemplo de collect

--- src/collect/colletPessoa.java
+++ src/collect/colletPessoa.java
@@ -3,11 +3,24 @@ package collect;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class colletPessoa {
     public static void main(String[] args) {
         System.out.println("Usando Collect");
+
+        Pessoa lista = new Pessoa();
+
+        Map<String, List<Pessoa>> collectStream = lista.listaDePessoas().stream()
+                .collect(Collectors.groupingBy(p -> p.getPremium() ? "sim" : "não"));
+        for(Pessoa p : collectStream.get("sim")) {
+            System.out.println("Clientes Premium: " + p);
+        }
+
+        System.out.println(" ");
+
+        for(Pessoa p : collectStream.get("não")) {
+            System.out.println("Clientes Free: " + p);
+        }
 
     }
 }
