4a1913756890eb21f5cdecce94a7df78158b75b9
dowglasOficial <bittencourtdouglas41@gmail.com>
2025-08-07 20:21:29 -0300

    abstração do reduce com o método sum

--- src/collect/ColletPessoa.java
+++ src/collect/ColletPessoa.java
@@ -4,7 +4,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-public class colletPessoa {
+public class ColletPessoa {
     public static void main(String[] args) {
         System.out.println("Usando Collect");
 
--- src/reduce/ReducePessoa.java
+++ src/reduce/ReducePessoa.java
@@ -1,4 +1,17 @@
 package reduce;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class ReducePessoa {
+    public static void main(String[] args) {
+        System.out.println("Exemplo Reduce");
+
+        List<Integer> listaDeNumeros = new LinkedList<>(List.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10));
+
+        Integer exemploReduce = listaDeNumeros.stream()
+                .reduce(0, Integer::sum);
+
+        System.out.println("exemploReduce: " + exemploReduce);
+    }
 }
